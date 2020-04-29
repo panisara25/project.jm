a0957c96ee38684df56cec14bcf7e5058dd6608a
panisara25 <32447136+panisara25@users.noreply.github.com>
2020-04-30 03:10:22 +0700

    initial commit

--- project/settings.py
+++ project/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteMiddleware'
 ]
 
 ROOT_URLCONF = 'project.urls'
